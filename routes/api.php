f86db9aae767c4417c04a58079384bfd43f03230
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2019-05-24 16:05:48 +0200

    Add Routes for login

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,20 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['jwt.verify', 'cors.enable']], function() {
+    Route::get('user', 'UserController@getAuthenticatedUser');
+
+    // Testding voor reactapp
+    Route::get('users/list', function(){
+        $users = App\User::all();
+
+        $response = ['success'=>true, 'data'=>$users];
+        return response()->json($response, 201);
+    });
+});
+
+Route::group(['middleware' => ['cors.enable']], function() {
+    Route::post('register', 'UserController@register');
+    Route::post('login', 'UserController@authenticate');
+});
